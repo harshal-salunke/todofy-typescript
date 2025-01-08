78f7b37dd7137cb98b11f61584681a45aeae149f
harshal-salunke <harshalsalunke23@gmail.com>
2025-01-09 01:24:38 +0530

    updated changes for all project  functionlity done

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,29 @@ import React, { useState } from 'react'
 import "./App.css"
 import InputField from './components/InputField'
 import { Todo } from './model';
+import TodoList from './components/TodoList';
 
 const App: React.FC = () => {
 
   const [todo, setTodo] = useState<string>("");
   const [todos, setTodos] = useState<Todo[]>([]); // This is how you can create array of type or an interface..
-  console.log(todo)
+
+  const handleAdd =(e:React.FormEvent)=> {
+    e.preventDefault();
+    if(todo){
+      setTodos([...todos, {id:Date.now(), todo, isDone: false}])
+      setTodo("")
+    }
+  }
+  console.log(todos)
 
   return (
     <div style={{backgroundColor: "#2f74c0"}}>
       <span className="heading">
         Taskify
       </span>
-      <InputField todo={todo} setTodo={setTodo}/>
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
+      <TodoList todos={todos} setTodos={setTodos}/> 
     </div>
   )
 }
